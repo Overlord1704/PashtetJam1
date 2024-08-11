273cc21c1d0afae31555063953b1a6a9f4aebc7a
Utopotam <utopotam@gmail.com>
2024-08-11 15:02:19 +0300

    установка труб на места

--- scenes/Level/Pipe.gd
+++ scenes/Level/Pipe.gd
@@ -3,5 +3,8 @@ class_name _TakeObject
 
 @export var active : bool
 
+var type: String = ''
+var code: String = ''
+
 func _physics_process(delta):
 	$Area2D.monitorable = active
--- scenes/Level/Place.gd
+++ scenes/Level/Place.gd
@@ -1,2 +1,21 @@
 extends Area2D
 class_name _PlaceObject
+
+var type: String = ''
+var code: String = ''
+
+var _sprite
+
+func set_sprite(sprite):
+	_sprite = sprite
+	add_child(_sprite)
+
+func check(other: String):
+	return other == type
+
+func install_pipe(pipe: _TakeObject):
+	if pipe.type == type and not _sprite.visible:
+		_sprite.show()
+		return true
+	else:
+		return false
