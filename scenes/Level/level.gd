b6af5b1674a3da8dc56ebdd9092ff8841bd08e64
unknown <vladislav>
2024-08-11 12:40:48 +0300

    Функция установки труб

--- scenes/Level/level.gd
+++ scenes/Level/level.gd
@@ -2,16 +2,33 @@ extends Node2D
 
 @export var player : CharacterBody2D
 
+var is_taked : bool # Подобрана?
+var is_placed : bool # Пололжил ли?
+
+var body_pipe : _TakeObject
+var place : _PlaceObject
+
 func _ready():
 	player.take_object.connect(Take)
 	player.place_object.connect(Place)
 	player.quit_game.connect(QuitGame)
-	
-func Take():
-	print("take")
 
-func Place():
-	print("place")
+func _physics_process(delta):
+	if is_taked:
+		body_pipe.position = player.position
+	if is_placed:
+		body_pipe.position = place.position
+		body_pipe.active = false
+		
+func Take(body : _TakeObject):
+	is_taked = true
+	body_pipe = body
+	
+func Place(zone_place : _PlaceObject):
+	if is_taked:
+		is_placed = true
+		place = zone_place
+		is_taked = false
 
 func QuitGame():
 	get_tree().change_scene_to_file("res://scenes/StartMenu/start_menu.tscn")
