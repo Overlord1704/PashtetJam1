997cf799d65826c494b04cf1d740a9e9bd0693f4
unknown <vladislav>
2024-08-11 18:53:11 +0300

    Полировка

--- scenes/Level/level.gd
+++ scenes/Level/level.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 
 @export var player : CharacterBody2D
+@export var take_sound : AudioStream
+@export var place_sound : AudioStream
 
 var is_taked : bool # Подобрана?
 var is_placed : bool # Пололжил ли?
@@ -20,11 +22,12 @@ func _ready():
 func _physics_process(delta):
 	if is_taked:
 		body_pipe.global_position = player.marker.global_position
+
 		#lerp(body_pipe.global_position, player.marker.global_position, delta)
 	if is_placed and body_pipe != null:
-		$PlacePipe.play(0)
 		body_pipe.global_position = place.global_position
 		current_fragments += 1
+
 	
 	if current_fragments >= Fragments:# Победное меню
 		get_tree().change_scene_to_file("res://scenes/WinnerMenu/winner.tscn")
@@ -32,17 +35,23 @@ func _physics_process(delta):
 func Take(body : _TakeObject):
 	if is_taked:
 		return
-	$TakePipe.play(0)
+	
 	is_taked = true
 	body_pipe = body
 	is_placed = false
-
+	TakeAndPlaceSound(take_sound)
+	
 func Place(zone_place : _PlaceObject):
 	if is_taked and zone_place.install_pipe(body_pipe):
 		is_placed = true
 		place = zone_place
 		is_taked = false
+		TakeAndPlaceSound(place_sound)
 		body_pipe.queue_free()
+
+func TakeAndPlaceSound(sound : AudioStream):
+	$Sound.stream = sound
+	$Sound.play(0)
 
 func QuitGame():
 	get_tree().change_scene_to_file("res://scenes/StartMenu/start_menu.tscn")
